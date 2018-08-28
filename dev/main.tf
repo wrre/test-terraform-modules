fba0305a2a2d9a2e4e65a5d9f4dbf19a3f055c75
Peggy Liu <fnoihtlanl@gmail.com>
2018-08-28 09:14:57 +0800

    Test 1

--- dev/main.tf
+++ dev/main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region = "${var.aws_region}"
 }
 
 resource "aws_s3_bucket" "dev" {
--- prod/main.tf
+++ prod/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "${var.aws_region}"
+
 }
 
 resource "aws_s3_bucket" "prod" {
